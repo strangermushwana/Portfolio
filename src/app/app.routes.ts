7ae0b3119399d3b04c644108a2dafac82f1741bf
Stranger <strangermushwana@gmail.com>
2024-06-07 14:12:10 +0200

    added contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,16 @@ import { Routes } from '@angular/router'
 import { HomepageComponent } from './homepage/homepage.component'
 import { AboutComponent } from './about/about.component'
 import { ProjectsComponent } from './projects/projects.component'
+import { ContactComponent } from './contact/contact.component'
+import { WorkComponent } from './work/work.component'
 
 export const routes: Routes = [
     { path: '', component: HomepageComponent},
     { path: 'about', component: AboutComponent},
-    // { path: 'contact', component: AppComponent},
+    { path: 'contact', component: ContactComponent},
     { path: 'projects', component: ProjectsComponent},
     // { path: 'blog', component: AppComponent},
-    // { path: 'work', component: AppComponent}
+    { path: 'work', component: WorkComponent},
     {
       path: '**',
       redirectTo: ''
