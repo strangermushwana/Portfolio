5a5fe238422d590d48e8fe2b2005bb9bf68e62e8
Stranger <strangermushwana@gmail.com>
2024-06-07 07:08:44 +0200

    added about and projects

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router'
 import { HomepageComponent } from './homepage/homepage.component'
+import { AboutComponent } from './about/about.component'
+import { ProjectsComponent } from './projects/projects.component'
 
 export const routes: Routes = [
     { path: '', component: HomepageComponent},
-    // { path: 'about', component: AppComponent},
+    { path: 'about', component: AboutComponent},
     // { path: 'contact', component: AppComponent},
-    // { path: 'portfolio', component: AppComponent},
-    // { path: 'skills', component: AppComponent},
+    { path: 'projects', component: ProjectsComponent},
+    // { path: 'blog', component: AppComponent},
     // { path: 'work', component: AppComponent}
     {
       path: '**',
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,22 +1,42 @@
-import { DOCUMENT } from '@angular/common'
-import { Component, Inject } from '@angular/core'
+import { CommonModule, DOCUMENT } from '@angular/common'
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core'
+import { Router, RouterModule } from '@angular/router'
+import { Subject, takeUntil, tap } from 'rxjs'
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
 
-  constructor(@Inject(DOCUMENT) private document: Document) {}
+  $unsubscribe: Subject<void> = new Subject()
+
+  activeRoute: string = ''
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      takeUntil(this.$unsubscribe),
+      tap((res:any) => this.activeRoute = res?.url),
+    ).subscribe()
+  }
 
   toggleNav(): void {
     const btn = this.document.querySelector('.mobile-menu-button')
     const menu = this.document.querySelector('.navigation-menu')
     btn?.classList.toggle('hidden')
     menu?.classList.toggle('hidden')
+  }
+
+  ngOnDestroy(): void {
+    this.$unsubscribe.unsubscribe()
   }
 
 }
